37167ebfeab334b0db6bcbfdf8d97c62a94d7755
Drake Qiu <adidasqiu@hotmail.com>
2020-10-21 09:58:27 +0800

    learn variable

--- basic/basic.go
+++ basic/basic.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 //var aa = 3
 //var ss = "kkk"
@@ -35,6 +39,27 @@ func variableShorter() {
 	fmt.Println(a, b, c, s)
 }
 
+func euler() {
+	//c := 3 + 4i
+	//fmt.Println(cmplx.Abs(c))
+
+	//fmt.Println(
+	//	cmplx.Pow(math.E, 1i * math.Pi) + 1)
+
+	//fmt.Println(
+	//	cmplx.Exp(1i * math.Pi) + 1)
+
+	fmt.Printf("%.3f\n",
+		cmplx.Exp(1i*math.Pi)+1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
 func main() {
 	fmt.Println("Hello world!")
 	variableZeroValue()
@@ -42,4 +67,6 @@ func main() {
 	variableTypeDeduction()
 	variableShorter()
 	fmt.Println(aa, ss, bb)
+	euler()
+	triangle()
 }
