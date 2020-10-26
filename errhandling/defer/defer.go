a0c86a151da684a6677b812f4886668778b58882
Drake Qiu <adidasqiu@hotmail.com>
2020-10-26 08:34:28 +0800

    learn error handling.

--- errhandling/defer/defer.go
+++ errhandling/defer/defer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"github.com/drakeqiu/learngo/functional/fib"
 	"os"
@@ -28,6 +29,31 @@ func writeFile(filename string) {
 	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	f := fib.Fibonacci()
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, f())
+	}
+}
+
+func writeFileFail(filename string) {
+	file, err := os.OpenFile(
+		filename, os.O_EXCL|os.O_CREATE, 0666)
+	err = errors.New("this is a custom error")
+	if err != nil {
+		if pathError, ok := err.(*os.PathError); !ok {
+			panic(err)
+		} else {
+			fmt.Printf("%s, %s %s\n", pathError.Op,
+				pathError.Path,
+				pathError.Err)
+		}
+		return
 	}
 	defer file.Close()
 
